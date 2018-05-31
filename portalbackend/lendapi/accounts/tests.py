0f2785f2dc2a48d1fc303cdf514e3cec732845f7
ixmk <muthukumar.s@ionixxtech.com>
2018-05-31 16:04:33 +0530

    bandit testing added

--- portalbackend/lendapi/accounts/tests.py
+++ portalbackend/lendapi/accounts/tests.py
@@ -1,55 +1,50 @@
-from portalbackend.settings import RUN_UNIT_TEST
+from tests.accounts.testcases import tc_user, tc_company, tc_contact, tc_espresso_contact, tc_change_password, \
+    tc_sceduled_maintaince, tc_two_factor, tc_login_logout, tc_forms
 
+# @unittest - Tests User List api calls
+tc_user_list_view = tc_user._001_UserListTestCase
 
-if RUN_UNIT_TEST is True:
+# @unittest - Tests Me api calls
+tc_user_me_view = tc_user._002_MeTestCase
 
-    from tests.accounts.testcases import tc_user, tc_company, tc_contact, tc_espresso_contact, tc_change_password, \
-        tc_sceduled_maintaince, tc_two_factor, tc_login_logout, tc_forms
+# @unittest - Tests User Details api calls
+tc_user_detail_view = tc_user._003_UserDetailsTestCase
 
-    # @unittest - Tests User List api calls
-    tc_user_list_view = tc_user._001_UserListTestCase
+# @unittest - Tests Company List api calls
+tc_company_list_view = tc_company._001_CompanyListTestCase
 
-    # @unittest - Tests Me api calls
-    tc_user_me_view = tc_user._002_MeTestCase
+# @unittest - Tests Company Details api calls
+tc_company_details_view = tc_company._002_CompanyDetailsTestCase
 
-    # @unittest - Tests User Details api calls
-    tc_user_detail_view = tc_user._003_UserDetailsTestCase
+# @unittest - Tests Company Meta api calls
+tc_company_meta_view = tc_company._003_CompanyMetaDetailsTestCase
 
-    # @unittest - Tests Company List api calls
-    tc_company_list_view = tc_company._001_CompanyListTestCase
+# @unittest - Tests Contact Details api calls
+tc_contact_view = tc_contact._001_ContactDetailsTestCase
 
-    # @unittest - Tests Company Details api calls
-    tc_company_details_view = tc_company._002_CompanyDetailsTestCase
+# @unittest - Tests Espresso Contact api calls
+tc_espresso_contact_view = tc_espresso_contact._001_EspressoContactsTestCase
 
-    # @unittest - Tests Company Meta api calls
-    tc_company_meta_view = tc_company._003_CompanyMetaDetailsTestCase
+# @unittest - Tests Email validation api calls
+tc_email_validation_view = tc_change_password._001_EmailValidationTestCase
 
-    # @unittest - Tests Contact Details api calls
-    tc_contact_view = tc_contact._001_ContactDetailsTestCase
+# @unittest - Tests Forgot password api calls
+tc_forgot_password_view = tc_change_password._002_ForgotPasswordTestCase
 
-    # @unittest - Tests Espresso Contact api calls
-    tc_espresso_contact_view = tc_espresso_contact._001_EspressoContactsTestCase
+# @unittest - Tests Change password api calls
+tc_change_password_view = tc_change_password._003_ChangePasswordTestCase
 
-    # @unittest - Tests Email validation api calls
-    tc_email_validation_view = tc_change_password._001_EmailValidationTestCase
+# @unittest - Tests Scheduled Maintenance Details api calls
+tc_sceduled_maintaince_view = tc_sceduled_maintaince._001_ScheduledMaintenanceDetailsTestCase
 
-    # @unittest - Tests Forgot password api calls
-    tc_forgot_password_view = tc_change_password._002_ForgotPasswordTestCase
+# @unittest - Tests Two Factor Authentication api calls
+tc_two_factor_view = tc_two_factor._001_TwoFactorAuthenticationDetailsTestCase
 
-    # @unittest - Tests Change password api calls
-    tc_change_password_view = tc_change_password._003_ChangePasswordTestCase
+# @unittest - Tests Login api calls
+tc_login_view = tc_login_logout._001_LoginViewTestCase
 
-    # @unittest - Tests Scheduled Maintenance Details api calls
-    tc_sceduled_maintaince_view = tc_sceduled_maintaince._001_ScheduledMaintenanceDetailsTestCase
+# @unittest - Tests Logout api calls
+tc_logout_view = tc_login_logout._002_LogoutTestCase
 
-    # @unittest - Tests Two Factor Authentication api calls
-    tc_two_factor_view = tc_two_factor._001_TwoFactorAuthenticationDetailsTestCase
-
-    # @unittest - Tests Login api calls
-    tc_login_view = tc_login_logout._001_LoginViewTestCase
-
-    # @unittest - Tests Logout api calls
-    tc_logout_view = tc_login_logout._002_LogoutTestCase
-
-    # @unittest - Tests Two Factor Authentication api calls
-    form_test = tc_forms.AccountsFormTest
+# @unittest - Tests Two Factor Authentication api calls
+form_test = tc_forms.AccountsFormTest
