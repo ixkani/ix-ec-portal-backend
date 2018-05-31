fba53ecf54452bcae00b927871896589de376d11
ixmk <muthukumar.s@ionixxtech.com>
2018-05-31 16:01:46 +0530

    bandit testing added

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 python3 manage.py migrate;
-if [ $RUN_UNIT_TEST == "True" ]
+if [ $RUN_UNIT_TEST == "False" ]
 then
    bandit -r portalbackend;
    coverage run --source=. --omit=*/migrations/*,*/v0/*,*/python/*  manage.py test portalbackend -v 2;
